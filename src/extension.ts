9f5c8039c27d31e1921c928495ce62745aac6842
Paige Haskins <phaskins@multistate.us>
2018-07-27 14:25:04 -0400

    Added Python support to extension

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 let grammarPaths = {
-  'source.ruby': '../syntaxes/ruby.tmLanguage.json'
+  'source.ruby': '../syntaxes/ruby.tmLanguage.json',
+  'source.python': '../syntaxes/MagicPython.tmLanguage.json'
 };
 
 let registry = new vsctm.Registry({
@@ -56,6 +57,10 @@ export function activate(context: vscode.ExtensionContext) {
 
       if (range) {
         registry.loadGrammar('source.ruby').then(grammar => {
+          if (!grammar) {
+            console.error('Grammar is null!');
+          }
+
           let word = editor.document.getText(range);
 
           // Check if the cursor is placed on a word, or if a word is selected
@@ -68,7 +73,7 @@ export function activate(context: vscode.ExtensionContext) {
             const regExp = new RegExp(/(\bend\b|\}|\{)/);
             const regExp1 = new RegExp(/\b(if|unless|while|until)\b/);
             const regExp2 = new RegExp(/\b(begin|case|class|def|do|for|module)\b/);
-            const regExp3 = new RegExp(/^\s*(if|unless|while|until)/);
+            const regExp3 = new RegExp(/^\s*\b(if|unless|while|until)\b/);
 
             // The line number returned is off by 1 (line numbers start at 0 instead of at 1, like in the editor)
             for (let lineNumber = editor.selection.start.line; lineNumber >= 0; lineNumber--) {
@@ -199,10 +204,6 @@ export function activate(context: vscode.ExtensionContext) {
                 }
                 
               } else if (regExp1.test(lineText) || regExp2.test(lineText) || lineText.match(/\{/)) {
-                  if (!grammar) {
-                    console.error('Grammar is null!');
-                  }
-
                   let lineTokens = grammar.tokenizeLine(lineText, null);
                   let inCommentOrString = 0;
                   let isAKeyword = -1;
@@ -266,7 +267,7 @@ export function activate(context: vscode.ExtensionContext) {
             }
 
           } else {
-            vscode.window.showInformationMessage('Please place the cursor on or select only the word "end"');
+            vscode.window.showInformationMessage('Please place the cursor on or select only word characters in a line');
           }
 
         }); 
@@ -277,7 +278,124 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
+  let disposable2 = vscode.commands.registerCommand('extension.showStartOfScope', () => {
+    // If the line is already highlighted, do nothing
+    if (decorationType) {
+      // vscode.window.showInformationMessage('Line is already highlighted');
+      return;
+    }
+
+    const editor = vscode.window.activeTextEditor;
+
+    if (editor) {
+      let range;
+
+      if (editor.selection.isEmpty) {
+        range = editor.document.getWordRangeAtPosition(editor.selection.start);
+
+      } else {
+        let startPos = editor.selection.start;
+        let endPos = editor.selection.end;
+        range = new vscode.Range(startPos, endPos);
+      }
+
+      if (range) {
+        registry.loadGrammar('source.python').then(grammar => {
+          if (!grammar) {
+            console.error('Grammar is null!');
+          }
+
+          let word = editor.document.getText(range);
+
+          // Check if the cursor is placed on a word, or if a word is selected
+          if (word) {
+            let lineNumber = editor.selection.start.line;
+            let lineText = editor.document.lineAt(lineNumber).text;
+            let indentSizeInSpaces: number = +editor.options.tabSize;
+            let spacesBeforeStartingLine = 0;
+            const regExp1 = new RegExp(/\b(class|def|elif|else|except|finally|for|if|try|while)\b/);
+            const regExp2 = new RegExp(/^\s*\b(class|def|elif|else|except|finally|for|if|try|while)\b/);
+
+            for (let i = 0; i < lineText.length; i++) {
+              if (lineText[i].match(/\s/)) {
+                spacesBeforeStartingLine++;
+              } else {
+                break;
+              }
+            }
+
+            let spacesBeforeTargetLine = spacesBeforeStartingLine - indentSizeInSpaces;
+
+            if (spacesBeforeTargetLine >= 0) {
+              // Regex to find lines with target number of indent spaces (spacesBeforeStartingLine - indentSizeSpaces)
+              // Want to find lines with scope that's one indent previous to the starting line
+              // i.e. ^\s{4}\S+
+              let regExpString = "^\\s{" + spacesBeforeTargetLine.toString() + "}\\S+";
+              let regExp = new RegExp(regExpString);
+
+              for (lineNumber = editor.selection.start.line; lineNumber >= 0; lineNumber--) {
+                lineText = editor.document.lineAt(lineNumber).text;
+
+                // If the current line has the indentation we're looking for...
+                if (regExp.test(lineText)) {
+                  // And matches one of the keywords we're looking for...
+                  if (regExp1.test(lineText)) {
+                    let lineTokens = grammar.tokenizeLine(lineText, null);
+                    // Go through each token in the line to find the keyword. Check that it's not part of a comment or string
+                    // scope, and that it's scope list contains the word "keyword"
+                    for (let i = 0; i < lineTokens.tokens.length; i++) {
+                      let startScopeInCommentOrString = -1;
+                      let isAKeyword = -1;
+                      let token = lineTokens.tokens[i];
+                      let tokenString = (lineText.substring(token.startIndex, token.endIndex));
+                      
+                      if (regExp1.test(tokenString)) {
+                        startScopeInCommentOrString = 0;
+
+                        for (let i = 0; i < token.scopes.length; i++) {
+                          if (token.scopes[i].includes('comment') || token.scopes[i].includes('string')) {
+                            startScopeInCommentOrString = 1;
+                            break;
+                          }
+                          // Check to make sure the token is a keyword
+                          if (token.scopes[i].includes('keyword')) {
+                            isAKeyword = 0;
+                          }
+                        }
+
+                        // If the correct line and token has been found, if the line is not a single line conditional/loop,
+                        // highlight the line and return
+                        if (startScopeInCommentOrString == 0 && isAKeyword == 0) {
+                          if (regExp2.test(lineText)) {
+                            addDecorations(editor, lineText, lineNumber);
+                            return;
+                          }
+                        } else {
+                          continue;
+                        }
+                      }
+                    }
+                  }
+                  
+                } else {
+                  continue;
+                }
+              }
+            }
+
+          } else {
+            vscode.window.showInformationMessage('Please place the cursor on or select only word characters in a line');
+          }
+        });
+
+      } else {
+        vscode.window.showInformationMessage('Nothing is selected');
+      }
+    }
+  });
+
   context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable2);
 
   function addDecorations(editor: vscode.TextEditor, lineText: string, lineNumber: number) {
     // Get the color from the user settings. If the color code is invalid or the provided value is not a string,
