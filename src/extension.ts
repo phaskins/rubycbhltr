03724a094702f350fae1224e610efdf455875646
Paige Haskins <phaskins@multistate.us>
2018-07-31 11:56:37 -0400

    Fixed bugs dealing with multiline doc comments

--- src/extension.ts
+++ src/extension.ts
@@ -319,6 +319,7 @@ export function activate(context: vscode.ExtensionContext) {
             for (lineNumber = editor.selection.start.line; lineNumber >= 0; lineNumber--) {
               lineText = editor.document.lineAt(lineNumber).text;
 
+              // Don't match lines with nothing on it
               if (!lineText.match(/^\s*$/)) {
                 spacesBeforeTargetLine = lineText.search(/\S/);
 
@@ -328,17 +329,50 @@ export function activate(context: vscode.ExtensionContext) {
               }
             }
 
+            // Regex to find lines with target number of indent spaces (spacesBeforeTargetLine)
+            // i.e. ^\s{4}\S+
+            let regExpString = "^\\s{" + spacesBeforeTargetLine.toString() + "}\\S+";
+            let regExp = new RegExp(regExpString);
+
             if (spacesBeforeStartingLine > 0 && spacesBeforeTargetLine >= 0) {
-              // Regex to find lines with target number of indent spaces (spacesBeforeTargetLine)
-              // i.e. ^\s{4}\S+
-              let regExpString = "^\\s{" + spacesBeforeTargetLine.toString() + "}\\S+";
-              let regExp = new RegExp(regExpString);
 
               for (lineNumber = editor.selection.start.line; lineNumber >= 0; lineNumber--) {
                 lineText = editor.document.lineAt(lineNumber).text;
 
                 // If the current line has the indentation we're looking for...
                 if (regExp.test(lineText)) {
+
+                  // Decrement the target indent size (of the line we're looking for) when a multiline doc comment delimiter
+                  // is found. Need to do this here, in cases of (for example) when there is a multiline doc comment that has a
+                  // multiline string variable declaration right behind it. We want the indented string lines to match
+                  // the variable declaration lines, while we want the doc comment string lines to match the scope
+                  // previous to that. Now we need to find the number of spaces before the previous scope and update the
+                  // spacesBeforeTargetLine variable
+                  if (lineText.match(/^\s*[\"\']{3}/)) {
+                    let oldSpacesBeforeTargetLine = spacesBeforeTargetLine;
+
+                    for (let lineNum = lineNumber; lineNum >= 0; lineNum--) {
+                      let currentLineText = editor.document.lineAt(lineNum).text;
+
+                      // Don't match lines with nothing on it or with leading white space followed by doc comment delimiters
+                      if (!currentLineText.match(/^\s*$/) && !currentLineText.match(/^\s*[\"\']{3}/)) {
+                        spacesBeforeTargetLine = currentLineText.search(/\S/);
+
+                        if (spacesBeforeTargetLine < oldSpacesBeforeTargetLine && spacesBeforeTargetLine < spacesBeforeStartingLine) {
+                          // Reset these variables
+                          regExpString = "^\\s{" + spacesBeforeTargetLine.toString() + "}\\S+";
+                          regExp = new RegExp(regExpString);
+                          break;
+                        }
+                      }
+                    }
+
+                    // Now, only look for lines with the new target indent size
+                    if (!regExp.test(lineText)) {
+                      continue;
+                    }
+                  }
+
                   // And matches one of the keywords we're looking for...
                   if (regExp1.test(lineText)) {
                     let lineTokens = grammar.tokenizeLine(lineText, null);
@@ -379,8 +413,8 @@ export function activate(context: vscode.ExtensionContext) {
                   } 
                   
                   // Match lines inside of dictionaries to the right scope. If a keyword wasn't found in the line
-                  // check for parenthesis, braces, brackets, or backslash
-                  if (lineText.match(/(\{|\(|\[|\\)/)) {
+                  // check for parenthesis, braces, brackets, backslash, or variables with multiline comments as values
+                  if (lineText.match(/(\{|\(|\[|\\|^\s*\w+\s*=\s*[\"\']{3})/)) {
                     let lineTokens = grammar.tokenizeLine(lineText, null);
                     // Check that match is not part of a comment or string scope, and that it's scope list contains 
                     // the proper scope name
@@ -470,6 +504,32 @@ export function activate(context: vscode.ExtensionContext) {
                           }
                           // Check to make sure the token has the scope "punctuation.separator.continuation.line"
                           if (token.scopes[i].includes('punctuation.separator.continuation.line')) {
+                            isAKeyword = 0;
+                          }
+                        }
+
+                        if (inCommentOrString == 0 && isAKeyword == 0) {
+                          addDecorations(editor, lineText, lineNumber);
+                          return;
+
+                        } else {
+                          continue;
+                        }
+
+                      } else if (tokenString.match(/[\"\']{3}/)) {
+                        inCommentOrString = 0;
+
+                        // Don't check for string scope here because we're looking to match something like this:
+                        // s = ''' this is a very
+                        //     long string '''
+
+                        for (let i = 0; i < token.scopes.length; i++) {
+                          if (token.scopes[i].includes('comment')) {
+                            inCommentOrString = 1;
+                            break;
+                          }
+                          // Check to make sure the token has the scope "punctuation.definition.string.begin"
+                          if (token.scopes[i].includes('punctuation.definition.string.begin')) {
                             isAKeyword = 0;
                           }
                         }
